126aa441dc44a24ceff24e82d5b22b5762d7c65c
Piotr Nowinski <pitred007git@gmail.com>
2019-05-03 15:22:17 +0200

    Init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# zadanie_15_6 Kanban
+
+Publish
+
+https://pitred.github.io/zadanie_15_6/. 
---
+++ js/app.js
@@ -0,0 +1,59 @@
+// variables
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    'X-Client-Id': 'X-Client-Id',
+    'X-Auth-Token': 'X-Auth-Token'
+}
+
+fetch(baseUrl + '/board', {
+        headers: myHeaders
+    })
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (resp) {
+        setupColumns(resp.columns);
+    });
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+        var col = new Column(column.id, column.name);
+        board.addColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+    cards.forEach(function (card) {
+        var cardObj = new Card(card.id, card.name);
+        col.addCard(cardObj);
+    });
+}
+
+function generateTemplate(name, data, basicElement) {
+    var template = document.getElementById(name).innerHTML;
+    var element = document.createElement(basicElement || 'div');
+
+    Mustache.parse(template);
+    element.innerHTML = Mustache.render(template, data);
+
+    return element;
+}
+
+// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
+var todoColumn = new Column('Do zrobienia');
+var doingColumn = new Column('W trakcie');
+var doneColumn = new Column('Skończone');
+
+// DODAWANIE KOLUMN DO TABLICY
+board.createColumn(todoColumn);
+board.createColumn(doingColumn);
+board.createColumn(doneColumn);
+
+// TWORZENIE NOWYCH EGZEMPLARZY KART
+var card1 = new Card('Nowe zadanie');
+var card2 = new Card('stworzyc tablice kanban');
+
+// DODAWANIE KART DO KOLUMN
+todoColumn.createCard(card1);
+doingColumn.createCard(card2);
